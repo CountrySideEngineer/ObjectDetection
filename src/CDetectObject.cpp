ca13b0d76890844f05626b6111a3224cb1f8142e
CountrySideEngineer <deep-sleeper@nifty.com>
2018-06-09 06:55:08 +0900

    Update CDetectObject class. * Add Arguments to constructor about filter size, thresh of threshold, and max value. * Add member values which are used in detection. * Add member values of image to set image data created while object detection.

--- src/CDetectObject.cpp
+++ src/CDetectObject.cpp
@@ -14,6 +14,19 @@ using namespace std;
 using namespace cv;
 
 /**
+ * Constructor with default argument.
+ *
+ * @param[in]	FilterSize	Size of filter used in object detection.
+ * @param[in]	Thresh		Threshold value used to change image into binary format.
+ * @param[in]	MaxValue	Max value used to change image into binary format.
+ */
+CDetectObject::CDetectObject(int FilterSize, int Thresh, int MaxValue)
+	: mFilterSize((double)FilterSize)
+	, mThresh((double)Thresh)
+	, mMaxValue((double)MaxValue)
+{}
+
+/**
  * Detect and mark traffic object.
  *
  * @param[in]	Source image to scan.
@@ -22,20 +35,18 @@ using namespace cv;
  * @return	An image traffic signs are detected and marked.
  */
 Mat* CDetectObject::Find(const Mat* SrcImage, const Mat* DstImage) {
-	Mat Gray;
-	Mat Bin;
-	Mat Filtered;
 
 	/*
 	 * To accentuate the edge of border, convert the input image into
 	 * 2 value scale, black and white.
 	 */
-	cvtColor(*SrcImage, Gray, COLOR_BGR2GRAY);
-	blur(Gray, Filtered, Size(5, 5));
-	threshold(Filtered, Bin, 210, 255, THRESH_BINARY);
+	Size Filter(this->mFilterSize, this->mFilterSize);
+	cvtColor(*SrcImage, mGray, COLOR_BGR2GRAY);
+	blur(mGray, mFiltered, Filter);
+	threshold(mFiltered, mBin, this->mThresh, this->mMaxValue, THRESH_BINARY);
 
 	vector< vector<Point> > Contours;
-	findContours(Bin, Contours, CV_RETR_LIST, CHAIN_APPROX_SIMPLE);
+	findContours(mBin, Contours, CV_RETR_LIST, CHAIN_APPROX_SIMPLE);
 
 	SrcImage->copyTo(*DstImage);
 	for (unsigned int index = 0; index < Contours.size(); index++) {
@@ -43,4 +54,46 @@ Mat* CDetectObject::Find(const Mat* SrcImage, const Mat* DstImage) {
 	}
 
 	return (Mat*)DstImage;
+}
+
+/**
+ * Setup parameters used while object detection.
+ *
+ * @param[in]	Config	Key of field to set value.
+ * @param[in]	Value	Value to set the field identified by argument Config.
+ */
+void CDetectObject::SetConfig(DETECT_OBJECT_CONFIG Config, int Value) {
+	switch (Config) {
+	case DETECT_OBJECT_CONFIG_FIL_SIZE:
+		this->mFilterSize = Value;
+		break;
+
+	default:
+		cerr << "Invalid argument.";
+		throw invalid_argument("Invalid configuration field");
+		break;
+	}
+}
+
+/**
+ * Setup parameters used while object detection.
+ *
+ * @param[in]	Config	Key of field to set value.
+ * @param[in]	Value	Value to set the field identified by argument Config.
+ */
+void CDetectObject::SetConfig(DETECT_OBJECT_CONFIG Config, double Value) {
+	switch (Config) {
+	case DETECT_OBJECT_CONFIG_THRESH_THRESH:
+		this->mThresh = Value;
+		break;
+
+	case DETECT_OBJECT_CONFIG_THRESH_MAXVAL:
+		this->mMaxValue = Value;
+		break;
+
+	default:
+		cerr << "Invalid argument.";
+		throw invalid_argument("Invalid configuration field");
+		break;
+	}
 }
