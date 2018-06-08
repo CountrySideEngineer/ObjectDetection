764aedf724c456d12e82f1b5c87cfca35f442cab
CountrySideEngineer <deep-sleeper@nifty.com>
2018-06-09 05:56:23 +0900

    Change and modify code to use CImageWindow class to show image.:

--- src/DetectWhiteLine.cpp
+++ src/DetectWhiteLine.cpp
@@ -13,6 +13,7 @@
 #include "opencv/cv.hpp"
 #include "opencv2/highgui.hpp"
 #include "CDetectObject.h"
+#include "CImageWindow.h"
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -58,13 +59,15 @@ int main(int argc, char** argv) {
 	cv::Mat DetectedImage(VideoReader.getImageHeight(),
 			VideoReader.getImageWidth(),
 			(int)CV_8UC3);
+	CImageWindow CapImageWindow(string("Image window"));
+	CImageWindow DetectedImageWindow(string("Detected image window"));
 	for (int index = 0; index < VideoReader.getImageFrameNum(); index++) {
 
 		CaptureImgae = VideoReader.Read();
 		DetectObject.Find((const Mat*)(&CaptureImgae), (const Mat*)(&DetectedImage));
 
-		cv::imshow(cv::String("image window"), CaptureImgae);
-		cv::imshow(cv::String("detected image"), DetectedImage);
+		CapImageWindow.Show(CaptureImgae);
+		DetectedImageWindow.Show(DetectedImage);
 
 		VideoWriter.Write(DetectedImage);
 
@@ -72,8 +75,6 @@ int main(int argc, char** argv) {
 	}
 
 	cout << "End Program!" << endl;
-
-	cv::destroyAllWindows();
 
 	return 0;
 }
